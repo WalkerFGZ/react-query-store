47a77edac4724a7b955cd197e1d98f47afc4197e
Andres Gallego <andresfelipegallegozuleta@gmail.com>
2025-01-09 22:46:10 -0500

    add product API integration and product listing components

---
+++ src/products/services/actions.ts
@@ -0,0 +1,16 @@
+import { productsApi } from "../api/productsApi";
+import { type Product } from "../interfaces/product";
+
+interface GetProductsOptions {
+  filterKey?: string;
+}
+
+const sleep = (seconds: number) => {
+  return new Promise((resolve) => setTimeout(resolve, seconds));
+};
+export const getProducts = async ({ filterKey }: GetProductsOptions) => {
+  await sleep(2000);
+  const filterUrl = filterKey ? `category=${filterKey}` : "";
+  const { data } = await productsApi.get<Product[]>(`/products?${filterUrl}`);
+  return data;
+};
