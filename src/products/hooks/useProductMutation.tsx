cbfc8b9674c03cc8be40453e0484a2941b85e3e1
Andres Gallego <andresfelipegallegozuleta@gmail.com>
2025-01-12 21:56:27 -0500

    add product mutation hook to update product list on successful creation

--- src/products/hooks/useProductMutation.tsx
+++ src/products/hooks/useProductMutation.tsx
@@ -1,10 +1,23 @@
-import { productActions } from "..";
-import { useMutation } from "@tanstack/react-query";
+import { Product, productActions } from "..";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export const useProductMutation = () => {
+  const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: productActions.createProduct,
-    onSuccess: () => {},
+    onSuccess: (product) => {
+      //   queryClient.invalidateQueries({
+      //     queryKey: ["products", { filterKey: data.category }],
+      //   });
+      queryClient.setQueriesData<Product[]>(
+        ["products", { filterKey: product.category }],
+        (old) => {
+          if (!old) return [product];
+
+          return [...old, product];
+        }
+      );
+    },
     onSettled: () => {},
   });
 
